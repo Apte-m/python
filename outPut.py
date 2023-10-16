c9c880107e9867d6fe0e5473748f66cf575acff5
OdintsovAS1 <OdintsovAS1@it.mos.ru>
2023-10-16 21:49:43 +0300

    1

---
+++ outPut.py
@@ -0,0 +1,14 @@
+# Заданеи 1
+
+item = [] 
+for _ in range(3):
+    item.append(input())
+
+print("Это:", item[0],"Ему:", item[1],"Его зовут:", item[2],sep=' ')
+
+
+item = [] 
+for _ in range(3):
+    item.append(input())
+
+print(item[0], item[1], item[2],sep='=>')
