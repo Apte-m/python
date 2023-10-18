24bf0d5f18620534d35485848f98498c65580bc8
OdintsovAS1 <OdintsovAS1@it.mos.ru>
2023-10-18 16:16:18 +0300

    Boolean

---
+++ BooleanAndConditionOperation.py
@@ -0,0 +1,21 @@
+# item = int(input())
+
+# if item > 0:
+#     if item % 2 == 0:
+#         print("Положительное четное число")
+#     else:
+#         print("Положительное не четное число")
+# elif item == 0:
+#     print("Число равно нулю")
+# else:
+#     if item % 2 != 0:
+#         print("Отрицательное не четное число")
+#     else:
+#         print("Отрицательно  четное число")
+
+# item = input()
+
+# result = item.count("a") + item.count("e") + item.count("o") + item.count("i") +  item.count("u")
+
+# print(result)
+    
