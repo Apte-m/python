65c4ff50fcb3435502d8d64d5f49eed20b797429
OdintsovAS1 <OdintsovAS1@it.mos.ru>
2023-10-22 14:30:40 +0300

    set

---
+++ Set.py
@@ -0,0 +1,17 @@
+
+inputNumber = int(input())
+
+value  = set()
+result = 0 
+
+for n in range(inputNumber):
+ value = set(map(int, input().split()))
+print(len(value))
+
+valueOne = list(input().split())
+valueTwo = list(input().split())
+
+print(len(valueOne))
+print(len(valueTwo))
+
+
