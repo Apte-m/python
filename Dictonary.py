068dbe62807ebacfc8331e212b0bf3d257fc5ce5
OdintsovAS1 <OdintsovAS1@it.mos.ru>
2023-10-23 09:46:51 +0300

    dictonary

---
+++ Dictonary.py
@@ -0,0 +1,19 @@
+# pets = dict()
+
+# nameAnimal = input("name animal: ")
+
+# ageAnimal = input("age animal: ")
+
+# viewAnimal = input("view animal: ")
+
+# pets = {value:{"age":ageAnimal,"view":viewAnimal}}
+
+# print(pets)
+
+
+numer = dict()
+
+for n in range(-5,10):
+    numer[n] = n**n
+    
+print(numer)
